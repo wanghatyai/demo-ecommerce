a22245e827c7c07c448228c824ba48111e9970bf
WangHatyaiDev <wanghatyai@gmail.com>
2024-04-02 16:13:39 +0700

    add admin setting page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -14,6 +14,11 @@ export function MainNav({
 
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: 'Overview',
+            active: pathname === `/${params.storeId}`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Setting',
             active: pathname === `/${params.storeId}/settings`,
